b0677c46a88926a4fe6fb3dce1b4ec745d7f0fcf
Joe Burton <20073613+jobu95@users.noreply.github.com>
2019-09-15 19:58:48 -0700

    Add text scrolling, monaco ttf, communist manifesto

--- text.py
+++ text.py
@@ -1,71 +1,143 @@
 #!/usr/bin/env python
 
 import sys
+import time
 from PIL import Image, ImageColor, ImageDraw, ImageFont
 
+# TODO this is a priori knowledge. Query this from font object, somehow
+xCharSz = 6
+yCharSz = 10
+font = ImageFont.truetype('MONACO.TTF', yCharSz)
+
+# Render lines of text, where lines is an iterable of strings.
 # xAlignment \elem { 'LEFT', 'CENTER' }
 # yAlignment \elem { 'TOP', 'CENTER' }
-def renderText(lines, yCharSz=10, xAlignment='LEFT', yAlignment='TOP'):
+# yOffsetPx may be negative
+def renderLines(image, lines, yCharSz=10, xAlignment='LEFT', yAlignment='TOP', yOffsetPx=0):
 
     draw = ImageDraw.Draw(image)
 
-    # max chars per line: 12
-    # max lines: ????
-    xCharSz = 6
-    yCharSz = 10
-    font = ImageFont.truetype('MONACO.TTF', yCharSz)
-    print(font)
-
     # Handle yAlignment
-    yOffset = 0
     xDim = image.size[0]
     yDim = image.size[1]
     if yAlignment is 'CENTER':
         numLines = min(len(lines), int(yDim / yCharSz))
         yOccupancyPx = numLines * yCharSz # number of y pixels the message occupies
         yMin = int((yDim - yOccupancyPx) / 2)
-        print("lines in this image: %d" % (numLines))
-        print("yOccupancyPx: %d" % (yOccupancyPx))
-        yOffset = int((yDim - yOccupancyPx) / 2)
-        print("yOffset: %d" % yOffset)
+        yOffsetPx += int((yDim - yOccupancyPx) / 2)
     elif yAlignment is 'TOP':
-        yOffset = 0
+        # Nothing to do
+        pass
     else:
         # TODO throw
         print("Invalid yAlignment")
         return
 
-    #numLines = max(len(lines),
-
     curline = 0
-    xoff = 0 # TODO implement horizontal text centering
     for line in lines:
-        xOffset = 0
+        xOffsetPx = 0
         if xAlignment is 'CENTER':
             numChars = min(len(line), int(xDim / xCharSz))
             xOccupancyPx = xCharSz * numChars
-            print("line: %s" % (line))
-            print("line len: %d" % (numChars))
-            print("line occupancy: %d" % (xOccupancyPx))
-            xOffset = int((xDim - xOccupancyPx) / 2)
-            print("xOffset: %d" % (xOffset))
+            xOffsetPx = int((xDim - xOccupancyPx) / 2)
+        elif xAlignment is 'LEFT':
+            # Nothing to do
+            pass
+        else:
+            # TODO throw
+            print("Invalid xAlignment")
 
-        draw.text((xOffset, yOffset + curline * yCharSz), line, font=font)
+        draw.text((xOffsetPx, yOffsetPx + curline * yCharSz), line, font=font)
         curline += 1
 
+def breakTextIntoLines(image, text, maxLines=-1):
+    xDim = image.size[0]
+    maxCharsPerLine = int(xDim / xCharSz)
+
+    lines = []
+
+    while len(text) > 0:
+
+        if len(text) <= maxCharsPerLine:
+            lines.append(text)
+            text = ""
+            continue
+
+        # Line needs to be broken
+        maxIdx = maxCharsPerLine
+
+        # Find last space
+        while text[maxIdx] != " " and maxIdx > 0:
+            maxIdx -= 1
+
+        if maxIdx == 0:
+            # Word is > maxCharsPerLine long. Check if it has a hyphen
+            maxIdx = maxCharsPerLine
+            while text[maxIdx] != "-" and maxIdx > 0:
+                maxIdx -= 1
+
+            if maxIdx == 0:
+                # Word does not contain a hyphen. Break it
+                maxIdx = maxCharsPerLine - 1
+                lines.append(text[:maxIdx] + "-")
+                text = text[maxIdx:]
+            else:
+                # Word has a hyphen
+                lines.append(text[:maxIdx + 1])
+                text = text[maxIdx + 1:]
+        else:
+            # Found a space
+            lines.append(text[:maxIdx])
+            text = text[maxIdx + 1:]
+
+        if maxLines > 0 and len(lines) == maxLines:
+            break
+
+    return lines
+
+# Render text from a string. Lines are automatically broken at whitespace, and
+# words are broken with hyphens if necessary.
+# This will generate as many images as necessary until the entire message has
+# been displayed.
+# * pause{Start,End}Frames control how many identical frames will be rendered at
+#   the start and end.
+# * yStepPx controls how many pixels each frame will shift up by while scrolling.
+def renderScrollingText(image, text, yStepPx=4, pauseStartFrames=4, pauseEndFrames=4):
+    lines = breakTextIntoLines(image, text)
+
+    start_image = image.copy()
+
+    yDim = image.size[1]
+    maxLinesPerScreen = int(yDim / yCharSz)
+
+    curYPx = 0
+    while True:
+        curLine = int(curYPx / yCharSz)
+        if curLine > len(lines):
+            break
+
+        line_slice = lines[curLine:curLine+maxLinesPerScreen+1]
+
+        image = start_image.copy()
+        yOffsetPx = -1 * (curYPx % yCharSz)
+        renderLines(image, line_slice, yOffsetPx=yOffsetPx)
+
+        if curLine % 7 == 0:
+            image.show()
+
+        curYPx += yStepPx
 
 xdim = 64
 ydim = 64
-
 image = Image.new('RGB', size=(xdim, ydim))
 
-# max chars per line: 12
-# max lines: 6
-text = ["I", "LOVE", "YOU"]
+renderScrollingText(image, "A spectre is haunting Europe - the spectre of communism. " +
+        "All the powers of old Europe have entered into a holy alliance to " +
+        "exorcise this spectre: Pope and Tsar, Metternich and Guizot, French " +
+        "Radicals and German police-spies.")
 
-renderText(text, xAlignment='CENTER', yAlignment='CENTER')
-
-image.show()
+#text = ["I", "LOVE", "YOU"]
+#renderLines(image, text, xAlignment='LEFT', yAlignment='TOP')
 
 #image.save(sys.stdout, "PNG")
 
