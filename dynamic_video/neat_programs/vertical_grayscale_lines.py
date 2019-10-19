f994296d2b37ff4b52dbaf1287a058c2e57775d9
Joe Burton <20073613+jobu95@users.noreply.github.com>
2019-10-19 15:49:43 -0700

    asdf

--- dynamic_video/neat_programs/vertical_grayscale_lines.py
+++ dynamic_video/neat_programs/vertical_grayscale_lines.py
@@ -11,16 +11,17 @@ chan = 3
 frame_count = 0
 frame_delay_ms = 16
 frame = np.zeros((xdim, ydim, chan), np.uint8)
+cv2.namedWindow('lines', cv2.WINDOW_NORMAL)
+cv2.resizeWindow('lines', 640, 640)
 while True:
     pxval = (frame_count % 256)
     frame_count += 1
-    print(pxval)
     for y in range(0,64):
         pxval += y*3
         for x in range(0,64):
             for c in range(0,3):
                 frame[x][y][c] = (pxval + x) % 256
-    cv2.imshow('asdf', frame)
+    cv2.imshow('lines', frame)
     if (cv2.waitKey(frame_delay_ms) & 0xFF) == ord('q'):
         break
 
