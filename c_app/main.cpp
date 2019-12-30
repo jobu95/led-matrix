2c060f1a7b38d90d07cd5692391ec391c99a5c6d
Joe Burton <20073613+jobu95@users.noreply.github.com>
2019-12-29 21:06:51 -0800

    fix transcoder bug, video playback is smooth

--- c_app/main.cpp
+++ c_app/main.cpp
@@ -2,6 +2,7 @@
 #include <content-streamer.h>
 #include <Magick++.h>
 
+#include <chrono>
 #include <errno.h>
 #include <fcntl.h>
 #include <filesystem>
@@ -14,20 +15,50 @@
 #include <unistd.h>
 
 using namespace std;
+using namespace std::chrono;
 using namespace Magick;
 using rgb_matrix::RGBMatrix;
 using rgb_matrix::FrameCanvas;
 
-list<filesystem::path> getFilesFromFilename(const string &filename)
+class FramePacer
+{
+  public:
+    FramePacer()
+      : m_now(high_resolution_clock::now())
+    {}
+
+    // Return true if we're on pace, false if we're behind by > 2 frames.
+    bool waitFrame(const microseconds frame_dur_us) {
+      bool on_pace = true;
+      while (true) {
+        auto now = high_resolution_clock::now();
+        auto delta_us = duration_cast<microseconds>(now - m_now);
+        if (delta_us > frame_dur_us) {
+          if (delta_us > 2 * frame_dur_us) {
+            on_pace = false;
+          }
+          break;
+        }
+        usleep(1000);
+      }
+      m_now += frame_dur_us;
+      return on_pace;
+    }
+
+  private:
+    high_resolution_clock::time_point m_now;
+};
+
+list<filesystem::path> getFilesFromFilename(const filesystem::path &filename)
 {
   list<filesystem::path> files;
-  if (filesystem::path filepath(filename); filesystem::exists(filepath)) {
-    files.push_back(move(filepath));
+  if (filesystem::exists(filename)) {
+    files.push_back(move(filename));
   } else {
     for (int i = 0; i < 1000; i++) {
       ostringstream oss;
-      oss << setfill('0') << setw(3) << i << "_" << filepath.filename().string();
-      filesystem::path cur_file(filepath);
+      oss << setfill('0') << setw(3) << i << "_" << filename.filename().string();
+      filesystem::path cur_file(filename);
       cur_file.replace_filename(oss.str());
       if (!filesystem::exists(cur_file)) {
         break;
@@ -39,38 +70,47 @@ list<filesystem::path> getFilesFromFilename(const string &filename)
 }
 
 volatile bool interrupt_received = false;
-static void InterruptHandler(int signo) {
+static void InterruptHandler(int /*signo*/) {
   interrupt_received = true;
 }
 
-list<Image> loadFile(const std::string &filename) {
-  list<Image> images;
+vector<Image> loadFile(const std::string &filename) {
+  list<Image> image_ls;
   try {
     // Read a file into image object
     cout << "Reading from " << filename << "... " << flush;
-    readImages(&images, filename);
+    readImages(&image_ls, filename);
     cout << "done" << endl;
   } catch (Exception &error_) {
     cout << "Caught exception: " << error_.what() << endl;
   }
-  if (images.size() > 0) {
-    cout << "  Width:           " << images.front().columns() << endl;
-    cout << "  Height:          " << images.front().rows() << endl;
-    cout << "  Depth:           " << images.front().depth() << endl;
-    cout << "  #   of frames:   " << images.size() << endl;
-    cout << "  Frame dur (ms):  " << images.front().animationDelay() * 10 << endl;
-    cout << "  Iterations:      " << images.front().animationIterations() << endl;
-    cout << "Converting to RGBMatrix format... " << flush;
+  if (image_ls.size() > 0) {
+    cout << "  Width:           " << image_ls.front().columns() << endl;
+    cout << "  Height:          " << image_ls.front().rows() << endl;
+    cout << "  Depth:           " << image_ls.front().depth() << endl;
+    cout << "  #   of frames:   " << image_ls.size() << endl;
+    cout << "  Frame dur (ms):  " << image_ls.front().animationDelay() * 10 << endl;
+    cout << "  Iterations:      " << image_ls.front().animationIterations() << endl;
   }
-  return images;
+  vector<Image> image_vec;
+  if (image_ls.size() > 1) {
+    cout << "Coalescing GIF frames... " << flush;
+    Magick::coalesceImages(&image_vec, image_ls.begin(), image_ls.end());
+    cout << "done" << endl;
+  } else {
+    image_vec.push_back(move(image_ls.front()));
+  }
+  return image_vec;
 }
 
-void sendImagesToStream(const list<Image> &images,
+void sendImagesToStream(const vector<Image> &images,
     FrameCanvas *canvas_off_p,
     rgb_matrix::StreamWriter *stream_writer_p) {
+  cout << "Converting to RGBMatrix format... " << flush;
   for (const auto &image : images) {
-    for (auto y = 0; y < image.rows(); y++) {
-      for (auto x = 0; x < image.columns(); x++) {
+    canvas_off_p->Clear(); // TODO is this necessary?
+    for (size_t y = 0; y < image.rows(); y++) {
+      for (size_t x = 0; x < image.columns(); x++) {
         const Magick::Color &c = image.pixelColor(x, y);
         canvas_off_p->SetPixel(x, y,
             c.quantumRed(),
@@ -78,7 +118,7 @@ void sendImagesToStream(const list<Image> &images,
             c.quantumBlue());
       }
     }
-    const auto delay_us = image.animationDelay() * 1000;
+    const auto delay_us = image.animationDelay() * 10000;
     stream_writer_p->Stream(*canvas_off_p, delay_us);
     if (interrupt_received) {
       break;
@@ -87,9 +127,31 @@ void sendImagesToStream(const list<Image> &images,
   cout << "done" << endl;
 }
 
-void convertFileToStreamFile(const std::string &file_in, const std::string &file_out) {
-  const auto images = loadFile(file_in);
+unique_ptr<RGBMatrix> createMatrix(char **argv, const bool convert_to_stream)
+{
+  unique_ptr<RGBMatrix> matrix_p(nullptr);
+  signal(SIGTERM, InterruptHandler);
+  signal(SIGINT, InterruptHandler);
 
+  RGBMatrix::Options matrix_options;
+  rgb_matrix::RuntimeOptions runtime_opt;
+  auto fake_argc = 1; // don't want to actually pass args to RGBMatrix
+  if (!rgb_matrix::ParseOptionsFromFlags(&fake_argc, &argv, &matrix_options,
+        &runtime_opt)) {
+    cerr << "Failed to parse args" << endl;
+    return matrix_p;
+  }
+
+  matrix_options.hardware_mapping = "adafruit-hat-pwm";  // or e.g. "adafruit-hat"
+  matrix_options.rows = 64;
+  matrix_options.cols = 64;
+  matrix_options.chain_length = 1;
+  matrix_options.parallel = 1;
+  matrix_options.show_refresh_rate = false;
+  runtime_opt.do_gpio_init = !convert_to_stream;
+  matrix_p.reset(CreateMatrixFromOptions(matrix_options, runtime_opt));
+
+  return matrix_p;
 }
 
 int main(int argc,char **argv)
@@ -98,62 +160,66 @@ int main(int argc,char **argv)
     cerr << "Usage: " << argv[0] << " filename" << endl;
     return 1;
   }
-  string filename(argv[1]);
-  cout << "Filename: " << filename << endl;
-
-  const auto files = getFilesFromFilename(filename);
+  filesystem::path filename(argv[1]);
+  cout << "Filename: " << filename.string() << endl;
+  const bool convert_to_stream = (filename.extension().string() != ".stream");
+  if (convert_to_stream) {
+    cout << "Converting file to .stream files" << endl;
+  } else {
+    cout << "Rendering to matrix" << endl;
+  }
 
   // Set up ImageMagick
   InitializeMagick(argv[0]);
 
   // Set up RGBMatrix
-  signal(SIGTERM, InterruptHandler);
-  signal(SIGINT, InterruptHandler);
-  unique_ptr<RGBMatrix> matrix_p(nullptr);
-  {
-    RGBMatrix::Options defaults;
-    defaults.hardware_mapping = "adafruit-hat-pwm";  // or e.g. "adafruit-hat"
-    defaults.rows = 64;
-    defaults.cols = 64;
-    defaults.chain_length = 1;
-    defaults.parallel = 1;
-    defaults.show_refresh_rate = false;
-    matrix_p.reset(rgb_matrix::CreateMatrixFromFlags(&argc, &argv, &defaults));
-    if (matrix_p == nullptr) {
-      return 1;
-    }
+  auto matrix_p = createMatrix(argv, convert_to_stream);
+  auto canvas_off_p = matrix_p->CreateFrameCanvas();
+  if (!matrix_p || !canvas_off_p) {
+    cerr << "Failed to get matrix or off-screen canvas" << endl;
   }
 
-  FrameCanvas *canvas_off_p = matrix_p->CreateFrameCanvas();
-
-  for (const auto &cur_file : files) {
+  for (const auto &cur_file : getFilesFromFilename(filename)) {
     if (interrupt_received) {
       break;
     }
-    // Convert PIL into RGBMatrix form
-    const auto images = loadFile(cur_file.string());
-    unique_ptr<rgb_matrix::StreamIO> stream_p;
-    if (true) {
-      stream_p = make_unique<rgb_matrix::MemStreamIO>();
-    } else {
-      const int stream_fd = open("out.stream", O_CREAT|O_WRONLY, 0644);
+    if (convert_to_stream) {
+      cerr << "Converting " << cur_file.string() << endl;
+      // Convert PIL into RGBMatrix form
+      auto new_file(cur_file);
+      new_file.replace_extension(".stream");
+      const int stream_fd = open(new_file.string().c_str(), O_CREAT|O_WRONLY, 0644);
       if (-1 == stream_fd) {
-        cerr << "Failed to create out.stream: " << strerror(errno) << endl;
+        cerr << "Failed to create " << new_file.string() << ": " << strerror(errno) << endl;
         return 1;
       }
-      stream_p = make_unique<rgb_matrix::FileStreamIO>(stream_fd);
-    }
-    auto stream_writer_p = make_unique<rgb_matrix::StreamWriter>(stream_p.get());
-
-    // TODO remove stream_writer_p raw ptr
-    sendImagesToStream(images, canvas_off_p, stream_writer_p.get());
-
-    rgb_matrix::StreamReader image_stream_reader(stream_p.get());
-    uint32_t delay_us = 0;
-    while (!interrupt_received &&
-        image_stream_reader.GetNext(canvas_off_p, &delay_us)) {
-      canvas_off_p = matrix_p->SwapOnVSync(canvas_off_p);
-      // TODO sleep until next frame
+      auto stream_p = make_unique<rgb_matrix::FileStreamIO>(stream_fd);
+      // Write out stream
+      auto stream_writer_p = make_unique<rgb_matrix::StreamWriter>(stream_p.get());
+      const auto images = loadFile(cur_file.string());
+      sendImagesToStream(images, canvas_off_p, stream_writer_p.get());
+    } else {
+      cerr << "Displaying " << cur_file.string() << endl;
+      // Open RGBMatrix stream file for reading
+      const int stream_fd = open(cur_file.string().c_str(), O_RDONLY);
+      if (-1 == stream_fd) {
+        cerr << "Failed to open " << cur_file.string() << ": " << strerror(errno) << endl;
+        return 1;
+      }
+      rgb_matrix::FileStreamIO stream(stream_fd);
+      // Display stream
+      rgb_matrix::StreamReader image_stream_reader(&stream);
+      uint32_t delay_us = 0;
+      class FramePacer pacer;
+      while (!interrupt_received &&
+          image_stream_reader.GetNext(canvas_off_p, &delay_us)) {
+        if (!pacer.waitFrame(microseconds(delay_us))) {
+          cerr << "skipping frame" << endl;
+          continue;
+        }
+        canvas_off_p = matrix_p->SwapOnVSync(canvas_off_p);
+        // TODO sleep until next frame
+      }
     }
   }
   return 0;
